78c750dd0e6904ad23dd35b58ea4008198ab89d6
jahhsm <realjahhsm@gmail.com>
2024-12-14 16:52:43 -0500

    xxxx

--- script.js
+++ script.js
@@ -58,21 +58,12 @@ document.addEventListener("DOMContentLoaded", () => {
                     link.textContent = file.free ? "Download" : "Purchase";
                     link.href = file.free ? file.download_url : file.purchase_url;
                     link.className = file.free ? "free" : "paid";
-
-                    const discord_link = document.createElement("a");
-                    
-                    link.textContent = file.discord_serverName || undefined;
-                    link.href = file.discord_server || undefined;
-                    link.className = "DiscordServer";
-
                     card.appendChild(img);
                     card.appendChild(title);
                     card.appendChild(tagsDiv);
                     card.appendChild(description);
                     card.appendChild(link);
 
-                    card.appendChild(discord_server);
-                    
                     fileContainer.appendChild(card);
                 });
             }
